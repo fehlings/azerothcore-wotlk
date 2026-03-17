2bf07a24d29023d17ea8764d690dd206768a7798
Luca Fehlings <fehlings97@gmail.com>
2026-03-17 20:31:14 +0100

    feat(Build/nix): add build and compose configuration
    This adds the build options for the azerothcore package and the
    compose options to start the database, set the db configuration, and
    to start the authserver and worldserver.

--- flake.nix
+++ flake.nix
@@ -4,15 +4,46 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    process-compose-flake.url = "github:xtian/process-compose-flake/patch-1";
+    services-flake.url    = "github:juspay/services-flake";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.process-compose-flake.flakeModule
+      ];
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells = {
-          default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          clientData = pkgs.fetchzip {
+            url       = "https://github.com/wowgaming/client-data/releases/download/v19/Data.zip";
+            hash      = "sha256-My6ZUYj5dze2rIyqJeOVUbHDUeZSsruiJsRWUDG391g=";
+            stripRoot = false;
+          };
+
+          azerothcore = pkgs.clangStdenv.mkDerivation {
+            pname   = "azerothcore";
+            version = "unstable";
+            src     = ./.;
+
+            nativeBuildInputs = with pkgs; [ cmake git ];
+            buildInputs = with pkgs; [ boost bzip2 mysql84 openssl readline zlib];
+
+            cmakeFlags = [
+              "-DCMAKE_INSTALL_PREFIX=$out/env/dist/"
+              "-DWITH_WARNINGS=1"
+              "-DTOOLS_BUILD=all"
+              "-DSCRIPTS=static"
+              "-DMODULES=static"
+            ];
+          };
+        in
+        {
+          packages.default = azerothcore;
+
+          devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
             nativeBuildInputs = with pkgs; [
               cmake
             ];
@@ -24,8 +55,62 @@
               bzip2
               zlib
             ];
+            AC_DATA_DIR = "${clientData}";
+          };
+
+          process-compose."dev" = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+            ];
+
+            services.mysql."acore-db" = {
+              enable    = true;
+              package   = pkgs.mysql84;
+              dataDir   = "./.mysql";
+
+              initialDatabases = [
+                { name = "acore_world"; }
+                { name = "acore_characters"; }
+                { name = "acore_auth"; }
+              ];
+
+              ensureUsers = [
+                  {
+                    name     = "acore";
+                    ensurePermissions = {
+                      "acore_world.*"      = "ALL PRIVILEGES";
+                      "acore_characters.*" = "ALL PRIVILEGES";
+                      "acore_auth.*"       = "ALL PRIVILEGES";
+                    };
+                  }
+              ];
+            };
+
+            settings.processes.authserver = {
+              command = "./env/dist/bin/authserver";
+              is_tty = true;
+              environment = {
+                AC_DATA_DIR = "${clientData}";
+                AC_LOGIN_DATABASE_INFO = "127.0.0.1;3306;acore;;acore_auth";
+                AC_WORLD_DATABASE_INFO = "127.0.0.1;3306;acore;;acore_world";
+                AC_CHARACTER_DATABASE_INFO = "127.0.0.1;3306;acore;;acore_characters";
+              };
+              depends_on."acore-db".condition = "process_healthy";
+            };
+
+            settings.processes.worldserver = {
+              command = "./env/dist/bin/worldserver";
+              is_interactive = true;
+              environment = {
+                AC_DATA_DIR = "${clientData}";
+                AC_LOGIN_DATABASE_INFO = "127.0.0.1;3306;acore;;acore_auth";
+                AC_WORLD_DATABASE_INFO = "127.0.0.1;3306;acore;;acore_world";
+                AC_CHARACTER_DATABASE_INFO = "127.0.0.1;3306;acore;;acore_characters";
+              };
+              depends_on."acore-db".condition   = "process_healthy";
+              depends_on."authserver".condition = "process_started";
+            };
           };
         };
-      };
     };
 }
